bc3b2063a218eab21bb05e8f2505be3bf16f5f17
jacksonnadar <jacksonnadar123@gmail.com>
2019-11-29 13:45:23 +0530

    new header

---
+++ quiz_questions.h
@@ -0,0 +1,81 @@
+char questionNo1(int i){
+    printf("%i: IC chips used in computers are usually made of:\n",i+1);
+    printf(" A. Lead\t\t\tB. Silicon\n");
+    printf(" C. Chromium\t\t\tD. Gold\n");
+    	int t=0;
+    char ch;
+
+	while(!kbhit())
+	{
+	   if(t>5)
+		{
+			printf("time up:\n");
+			break;
+		}
+		sleep(1);
+		t=t+1;	
+		if( kbhit())
+		{
+		
+	      	ch=getch();
+			printf("hii total time is %d",t);
+			printf("you hv entered %c",ch);
+			break;
+		}
+	}
+	return ch;
+}
+char questionNo2(int i){
+    printf("%i: IC chips used in computers are usually made of:\n",i+1);
+    printf(" A. Lead\t\t\tB. Silicon\n");
+    printf(" C. Chromium\t\t\tD. Gold\n");
+    	int t=0;
+    char ch;
+
+	while(!kbhit())
+	{
+	   if(t>5)
+		{
+			printf("time up:\n");
+			break;
+		}
+		sleep(1);
+		t=t+1;	
+		if( kbhit())
+		{
+		
+	      	ch=getch();
+			printf("hii total time is %d",t);
+			printf("you hv entered %c",ch);
+			break;
+		}
+	}
+	return ch;
+}
+char questionNo3(int i){
+    printf("%i: IC chips used in computers are usually made of:\n",i+1);
+    printf(" A. Lead\t\t\tB. Silicon\n");
+    printf(" C. Chromium\t\t\tD. Gold\n");
+    	int t=0;
+    char ch;
+
+	while(!kbhit())
+	{
+	   if(t>5)
+		{
+			printf("time up:\n");
+			break;
+		}
+		sleep(1);
+		t=t+1;	
+		if( kbhit())
+		{
+		
+	      	ch=getch();
+			printf("hii total time is %d",t);
+			printf("you hv entered %c",ch);
+			break;
+		}
+	}
+	return ch;
+}
